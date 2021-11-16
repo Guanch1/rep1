bfe692ef7e1421f89b67ae1ebe3c94f9e53616ea
Guanch1 <94456292+Guanch1@users.noreply.github.com>
2021-11-16 15:05:41 +0000

    Initialize dev

---
+++ frontend/global/ts/reload-scripts-on-content-refresh.ts
@@ -0,0 +1,30 @@
+declare var CMS: any;
+
+
+export function initReloadScriptsOnContentRefresh() {
+    const isCmsPresent = window.hasOwnProperty('CMS');
+    if (isCmsPresent) {
+        initScriptReloadListener();
+    }
+}
+
+
+function initScriptReloadListener() {
+    const cmsPageEditedEvent = 'cms-content-refresh';
+    CMS.$(window).on(cmsPageEditedEvent, () => {
+        $('script[data-is-reload-on-page-edit]').each((index, element) => {
+            forceScriptReload.call(element);
+        });
+        window.document.dispatchEvent(new Event('DOMContentLoaded', {
+            bubbles: true,
+            cancelable: true,
+        }));
+    });
+}
+
+
+function forceScriptReload() {
+    const scriptSrc = $(this).attr('src') as string;
+    $(this).remove();
+    $('<script>').attr('src', scriptSrc).appendTo('head');
+}
